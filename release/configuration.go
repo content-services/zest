049c3e1a6c99c8347826714bfa25df16cf9db566
andrewgdewar <andrewgdewar@gmail.com>
2024-10-09 07:10:04 +0000

    Update pulp bindings to e69db48356e528a464be3da896237bd3a2e69e1a7d54eb5892a9db36ae29d5a3

--- release/configuration.go
+++ release/configuration.go
@@ -93,7 +93,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/386d86254354e29d3b864523aed47863a3db8d167968b5e2da9ab863deab9a54/go",
+		UserAgent:        "OpenAPI-Generator/e69db48356e528a464be3da896237bd3a2e69e1a7d54eb5892a9db36ae29d5a3/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
